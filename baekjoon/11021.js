ab907ab083f1280fbf5224d7212eb2fddfd4446b
chunyong.kim <lovemy5854@naver.com>
2022-07-26 22:51:58 +0900

    feat: 11021ㅂㅓㄴ A+B-7

---
+++ baekjoon/11021.js
@@ -0,0 +1,10 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+let testcase = Number(input[0]);
+
+for (let i = 1; i <= testcase; i++) {
+    let A = Number(input[i].split(' ')[0]);
+    let B = Number(input[i].split(' ')[1]);
+    console.log(`Case #${i}: ${A + B}`);
+}
