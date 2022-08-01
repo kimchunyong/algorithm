1061d377a2cd46c2f1bf7439063e31162787f6d6
chunyong.kim <lovemy5854@naver.com>
2022-08-01 15:40:02 +0900

    feat: 2438번 별찍기

---
+++ baekjoon/2438.js
@@ -0,0 +1,23 @@
+const readline = require("readline")
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+})
+
+const input = []
+
+rl.on("line", function(line) {
+    input.push(line)
+}).on("close", function() {
+    const num = parseInt(input[0]);
+    let string = ''
+
+    for(let i = 1; i <= num; i++){
+        for(let j = i; j >0; j--){
+            string += '*'
+        }
+        string += '\n'
+    }
+    console.log(string)
+})
