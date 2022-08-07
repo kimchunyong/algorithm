5992e912d031b1419964296f820ef37eaf45d6cd
chunyong.kim <lovemy5854@naver.com>
2022-08-07 23:55:48 +0900

    feat: 10951번

---
+++ baekjoon/10951.js
@@ -0,0 +1,18 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const resultArr = [];
+rl.on('line', line => {
+    resultArr.push(line.split(' '))
+}).on('close', () => {
+    for(let i = 0; i < resultArr.length; i++){
+        resultArr[i].reduce((acc, cur) => {
+            console.log(+acc + +cur)
+        })
+    }
+
+    process.exit();
+})
